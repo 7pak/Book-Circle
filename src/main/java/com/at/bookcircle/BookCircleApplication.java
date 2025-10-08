1b4872dfea9d254652cac16e411461d5abef5f6d
abood <fatabdella7@gmail.com>
2025-10-08 14:49:52 +0400

    Implemented auditing for createdBy/updatedBy

--- src/main/java/com/at/bookcircle/BookCircleApplication.java
+++ src/main/java/com/at/bookcircle/BookCircleApplication.java
@@ -4,7 +4,6 @@ import com.at.bookcircle.role.Role;
 import com.at.bookcircle.role.RoleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableAsync
 public class BookCircleApplication {
 
--- src/main/java/com/at/bookcircle/config/BeanConfig.java
+++ src/main/java/com/at/bookcircle/config/BeanConfig.java
@@ -3,6 +3,7 @@ package com.at.bookcircle.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -24,6 +25,11 @@ public class BeanConfig {
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
+    }
+
+    @Bean
+    public AuditorAware<Integer> auditorAware() {
+        return new ApplicationAuditAware();
     }
 
     @Bean
